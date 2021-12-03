71e60348e7924033817b1ca522132e5f098afc3f
Ig0or <igorprovensi@gmail.com>
2021-12-03 16:00:15 -0300

    personalizado compos cadastro de usuario

--- admin_blog/models.py
+++ admin_blog/models.py
@@ -1,6 +1,18 @@
+from django.contrib.auth.base_user import AbstractBaseUser
 from django.db import models
 
 # Create your models here.
+
+class Usuario(AbstractBaseUser):
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    email = models.EmailField(unique=True, null=False, blank=False)
+    pais_origem = models.CharField(max_length=20, null=False, blank=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['nome', 'pais_origem']
+
+    def __str__(self):
+        return self.email
 
 class Categoria(models.Model):
     nome = models.CharField(max_length=30, null=False, blank=False)
