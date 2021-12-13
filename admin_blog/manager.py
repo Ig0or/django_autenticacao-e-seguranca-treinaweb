8e260c4ef0438bf0cb4f2cb8d085410f6234e39a
Ig0or <igorprovensi@gmail.com>
2021-12-13 10:11:25 -0300

    criado method superuser

--- admin_blog/manager.py
+++ admin_blog/manager.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.password_validation import password_changed
 
 class UsuarioManager(BaseUserManager):
     def create_user(self, email, nome=None, pais_origem=None, password=None):
@@ -8,7 +9,14 @@ class UsuarioManager(BaseUserManager):
             nome = nome,
             pais_origem = pais_origem,
             email = self.normalize_email(email)
-        )
+         )
+        usuario.set_password(password)
+        usuario.save()
+        return usuario
+
+    def create_superuser(self, nome, pais_origem, email, password):
+        usuario = self.create_user(nome=nome, email=email, pais_origem=pais_origem)
+        usuario.is_superuser = True
         usuario.set_password(password)
         usuario.save()
         return usuario
